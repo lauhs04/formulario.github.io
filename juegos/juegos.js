e35a31f22c4e5b4f52e190e5b1a17ce364232da6
laura <laura.hoyos.suarez@gmail.com>
2022-04-25 09:55:11 -0500

    añadiendo js juego

---
+++ juegos/juegos.js
@@ -0,0 +1,73 @@
+let numeroAleatorio = Math.floor(Math.random()*100)+1;
+
+const intentos = document.querySelector('.intentos');
+const ultimoIntento = document.querySelector('.ultimoIntento');
+const lowOrHi = document.querySelector('.lowOrHi');
+
+const campoEnviado = document.querySelector('.campoEnviado');
+const campoAdivinar = document.querySelector('.campoAdivinar');
+
+let contador = 1;
+let resetButton;
+
+function adivinar() {
+    let intentoJugador = Number(campoAdivinar.value);
+    if(contador===1){
+        intentos.textContent = 'Intentos anteriores:'
+    }
+    intentos.textContent += intentoJugador + ' ';
+
+     if (intentoJugador === numeroAleatorio) {
+         ultimoIntento.textContent = "Felicidades!! ¡Lo lograste!";
+         ultimoIntento.style.backgroundColor = 'violet';
+         lowOrHi.textContent= '';
+         setGameOver();
+     } else if (contador === 10){
+         ultimoIntento.textContent = "se acabaron los intentos";
+         setGameOver();
+     } else {
+         ultimoIntento.textContent = 'Incorrecto';
+         ultimoIntento.style.backgroundColor = 'red';
+         if(intentoJugador < numeroAleatorio){
+             lowOrHi.textContent= "el número es muy bajo";
+         } else if(intentoJugador > numeroAleatorio) {
+             lowOrHi.textContent = "el número es muy alto"
+         }
+     }
+
+     contador++;
+     campoAdivinar.value='';
+     campoAdivinar.focus();
+}
+
+campoEnviado.addEventListener('click', adivinar);
+
+function setGameOver() {
+    campoAdivinar.disabled = true;
+    campoEnviado.disabled = true;
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Iniciar nuevo juego';
+    document.body.append(resetButton);
+    resetButton.addEventListener('click', resetGame);
+  }
+
+function resetGame() {
+    contador = 1;
+
+    const resultados = 
+    document.querySelectorAll('.resultados p');
+    for (let i = 0; i < resultados.length ; i++)
+    {
+        resultados[i].textContent = '';
+    }
+
+    resetButton.parentNode.removeChild(resetButton);
+
+    campoAdivinar.disabled=false;
+    campoEnviado.disabled=false;
+    campoAdivinar.value='';
+    campoAdivinar.focus();
+
+    ultimoIntento.style.backgroundColor = 'white';
+    numeroAleatorio = Math.floor(Math.random()*100)+1;
+}
