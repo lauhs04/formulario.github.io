24a79fc8384363a66c55186cc1ef5559b140c3bc
laura <laura.hoyos.suarez@gmail.com>
2022-05-28 12:29:37 -0500

    añadiendo método fetch y carga asincrónica

--- fetch.js
+++ fetch.js
@@ -1,34 +1,41 @@
+/* Carga todo un evento en la  ventana window */
 window.onload = function () {
-    iniciarEventos();
+    iniciarEvent();
 }
 
-function iniciarEventos () {
+/*Busca a todos los elementos a los que se les debe aplicar un evento e inicia ese evento*/ 
+
+function iniciarEvent () {
     let obtenerDatos =document.getElementsByClassName("btnCarga");
 
-    for(i=0; i < obtenerDatos.length; i++){
-        obtenerDatos[i].addEventListener("click", cargarHtml)
+    for(let i=0; i < obtenerDatos.length; i++){
+        obtenerDatos[i].addEventListener("click", cargarHTML)
     }
 
-    function cargarHtml(event) {
-        let element = event.target;
-        let pagina = element.value;
-        let js = element.getAttribute("data-js");
+    function cargarHTML(event) {
+        //del evento generado se csptura el evento que lo disparó
 
+        let elemento = event.target;
+        //se obtiene el atributo value del elemento
+        let pagina = elemento.value;
+        let js = elemento.getAttribute("data-js");
+// con FETCH cargamos dinámicamente la página solicitada
         fetch(pagina)
 
         .then(function(repuesta){
             return repuesta.text();
-        })
+        })//esto devuelve otra promesa
 
-        .then(function(html){
-            document.getElementById("mains").innerHTML = html;
-            if (js != "NO"){
-                getScript("./juegos/" + js)
-                    .then(function(){
-                        iniciarEventos();
-                    })
-                .catch(function(error){
-                    console.log(error);
+            .then(function(html){
+            //buscamos el elemento html donde se va a cargar la pagina
+                document.getElementById("mains").innerHTML = html;
+                if (js !== "NO"){
+                    getScript("calculadora/" + js)
+                        .then(function(){
+                         iniciarEvent();
+                        })
+                        .catch(function(error){
+                        console.log(error);
                 })
             }
         })
@@ -40,7 +47,7 @@ function iniciarEventos () {
 }
 
 function getScript(url){
-    new Promise(function(resolve, reject){
+    return new Promise(function(resolve, reject){
         const script= document.createElement("script");
         script.src = url;
         script.async= true;
